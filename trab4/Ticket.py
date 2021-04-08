56287ee9f6c5a6f06d2212dcf1e21a835a90c445
LucasNolasco <lucasnolasco.5@gmail.com>
2021-04-08 19:24:06 -0300

    Atualizando com os ultimos trabalhos

---
+++ trab4/Ticket.py
@@ -0,0 +1,5 @@
+class Ticket:
+    def __init__(self, identidade, prazo, chave_sessao):
+        self.identidade = identidade
+        self.prazo = prazo
+        self.chave_sessao = chave_sessao
---
+++ trab4/cliente.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def menu():
+    ID_C = input("Insira o id do cliente: ")
+    ID_S = input("Insira o id do serviço: ")
+    T_R = input("Insira o prazo de validade para acesso ao serviço em segundos: ")
+    
+
+    return ID_C, ID_S, T_R
+
+def main():
+    pass
+
+if __name__=='__main__':
+    main()
