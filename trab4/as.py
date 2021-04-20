b7a83cfef118cf571069768bc8adfd5a65fa71c3
LucasNolasco <lucasnolasco.5@gmail.com>
2021-04-20 19:38:08 -0300

    M1 do kerberos adicionada

--- trab4/as.py
+++ trab4/as.py
@@ -1,2 +1,27 @@
 #!/usr/bin/python3
 
+from Crypto.Cipher import DES
+from flask import request, Flask
+app = Flask(__name__)
+
+senhas = {
+    'lucas': 'chave123'
+}
+
+@app.route("/authentication_service/authenticate")
+def client_authentication_request():
+    pedido = request.get_json()
+    usuario = pedido["ID_C"]
+    if usuario in senhas:
+        chave = senhas[usuario]
+        cifra_des = DES.new(chave.encode(), DES.MODE_ECB)
+        ID_S = cifra_des.decrypt(bytes.fromhex(pedido["ID_S"])).decode()
+        T_R = cifra_des.decrypt(bytes.fromhex(pedido["T_R"])).decode()
+        N1 = cifra_des.decrypt(bytes.fromhex(pedido["N1"])).decode()
+        
+        print(f"ID_S: {ID_S}, T_R: {T_R}, N1: {N1}")
+
+    return "Ok"
+
+if __name__ == "__main__":
+    app.run(port=5000)
--- trab4/cliente.py
+++ trab4/cliente.py
@@ -1,15 +1,50 @@
 #!/usr/bin/python3
+from Crypto.Cipher import DES
+import random
+import sys
+import requests
+
+AUTENTICATION_SERVICE_URL = "http://localhost:5000/authentication_service/authenticate"
+
+def pad(text):
+    while len(text) % 8 != 0:
+        text += '\x00'
+
+    return text
 
 def menu():
     ID_C = input("Insira o id do cliente: ")
     ID_S = input("Insira o id do serviço: ")
     T_R = input("Insira o prazo de validade para acesso ao serviço em segundos: ")
-    
 
-    return ID_C, ID_S, T_R
+    tamanho_chave_correto = False
+    while not tamanho_chave_correto:
+        key = input(f"Digite a chave para criptografar (A chave deverá ter exatamente {DES.key_size} caracteres): ")
+        tamanho_chave_correto = True
+        if len(key) == DES.key_size:
+            tamanho_chave_correto = True
+
+    return ID_C, ID_S, T_R, key
 
 def main():
-    pass
+    ID_C, ID_S, T_R, key = menu()
+
+    cifra_des = DES.new(key.encode(), DES.MODE_ECB)
+
+    ID_S = cifra_des.encrypt(pad(ID_S).encode())
+    T_R = cifra_des.encrypt(pad(T_R).encode())
+    N1 = random.randint(0, sys.maxsize)
+    print(f"N1: {N1}")
+    N1 = cifra_des.encrypt(pad(str(N1)).encode())
+
+    request_data = {
+        "ID_C": ID_C,
+        "ID_S": ID_S.hex(),
+        "T_R": T_R.hex(),
+        "N1": N1.hex()
+    }
+
+    requests.get(AUTENTICATION_SERVICE_URL, json=request_data)
 
 if __name__=='__main__':
     main()
