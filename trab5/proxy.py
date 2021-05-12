084e27170f58c3511b0fe57cecbee0cd1a02baf0
LucasNolasco <lucasnolasco.5@gmail.com>
2021-05-12 15:55:38 -0300

    Pagina com o alerta de monitorando adicionada

--- trab5/proxy.py
+++ trab5/proxy.py
@@ -4,6 +4,8 @@ import socket
 from signal import signal, SIGINT
 import sys
 
+SOCKET_TIMEOUT = 0.25
+
 class WebServer:
     def main(self):
         port = 5001
@@ -16,128 +18,74 @@ class WebServer:
         while True:
             (con, (ip, porta)) = tcp.accept()
                 
-            request_thread = HttpRequest(con)
+            request_thread = HttpRequest(con, ip)
             request_thread.start()
                 
 class HttpRequest(threading.Thread):
-    def __init__(self, request_socket):
+    def __init__(self, request_socket, cliente):
         threading.Thread.__init__(self)
         self.request_socket = request_socket
         self.CRLF = "\r\n"
+        self.cliente = cliente
+
+    def recvall(self, recv_socket):
+        buffer = b''
+        while True:
+            try:
+                dados = recv_socket.recv(4096)
+                buffer += dados
+                if not dados: # Se não tem mais nada a receber
+                    break
+            except:
+                break
+
+        return buffer
 
     def run(self):
-        self.request_socket.settimeout(1)
-        input_stream = b''
-        while True:
-            try:
-                stream_slice = self.request_socket.recv(4096)
-                input_stream += stream_slice
-                if len(stream_slice) < 4096: # Se não tem mais nada a receber
-                    break
-            except:
-                break
+        self.request_socket.settimeout(SOCKET_TIMEOUT)
+        request_original = self.recvall(self.request_socket)
 
-        input_stream = input_stream.decode()
+        original_host = request_original.decode().split(self.CRLF)[1].replace("Host: ", "")
+        objeto_requisitado = request_original.decode().split(self.CRLF)[0].split(" ")[1]
 
-        # input_stream = self.request_socket.recv(4096).decode()
-
-        # print(input_stream)
-        original_host = input_stream.split(self.CRLF)[1].replace("Host: ", "")
-        # print(original_host)
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(0.25)
-        s.connect((original_host, 80))
-        s.sendall(input_stream.encode())
-
-        data = b''
-        while True:
-            try:
-                stream_slice = s.recv(4096)
-                if not stream_slice:
-                    break
-                
-                data += stream_slice                
-            except:
-                break
-
-        # print(data)
-
-        s.shutdown(socket.SHUT_RDWR)
-        s.close()
-
-        self.request_socket.sendall(data)
-
-        self.request_socket.shutdown(socket.SHUT_RDWR)        
-        self.request_socket.close()
-
-    def processRequest(self):
-        input_stream = self.request_socket.recv(1024)
-
-        request_line = input_stream.split(self.CRLF)[0]
-
-        print(request_line)
-
-        if request_line.split(" ")[0] == "GET":
-            file_name = request_line.split(" ")[1]
-            file_name = os.getcwd() + file_name
-
-            file_exists = True
-            if(os.path.isfile(file_name)):
-                fis = open(file_name, "rb")
-            else:
-                file_exists = False
-
-            status_line = ""
-            content_type_line = ""
-            entity_body = ""
-            
-            if(file_exists):
-                status_line = "HTTP/1.0 200 OK"
-                content_type_line = "Content-Type: " + self.contentType(file_name)
-
-            else:
-                status_line = "HTTP/1.0 404 Not Found"
-                content_type_line = "Content-Type: text/html"
-                entity_body = "<HTML>" + "<HEAD><TITLE>Not Found</TITLE></HEAD>" + "<BODY>Not Found</BODY></HTML>"
+        if "monitorando" in objeto_requisitado.lower():
+            status_line = b"HTTP/1.0 200 OK"
+            content_type_line = b"Content-Type: text/html"
+            entity_body = "<HTML>" + "<HEAD><TITLE>Monitoramento</TITLE></HEAD>" + "<BODY>Acesso nao autorizado</BODY></HTML>"
 
             self.request_socket.send(status_line)
-            self.request_socket.send(self.CRLF)
+            self.request_socket.send(self.CRLF.encode())
             self.request_socket.send(content_type_line)
-            self.request_socket.send(self.CRLF)
+            self.request_socket.send(self.CRLF.encode())
+            self.request_socket.send(self.CRLF.encode())
+            self.request_socket.send(entity_body.encode())
 
-            self.request_socket.send(self.CRLF)
-
-            if(file_exists):
-                file_piece = fis.read(1024)
-                while(file_piece):
-                    self.request_socket.send(file_piece)
-                    file_piece = fis.read(1024)
-
-                fis.close()
-
-            else:
-                self.request_socket.send(entity_body)
+            codigo_resposta = 200
 
         else:
-            print("Falha na execucao")
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(SOCKET_TIMEOUT)
+            s.connect((original_host, 80))
+            s.sendall(request_original)
+
+            resposta_servidor = self.recvall(s)
+
+            s.shutdown(socket.SHUT_RDWR)
+            s.close()
+
+            self.request_socket.sendall(resposta_servidor)
+
+            codigo_resposta = resposta_servidor.split(self.CRLF.encode())[0].decode().split(" ")[1]
 
         self.request_socket.shutdown(socket.SHUT_RDWR)        
         self.request_socket.close()
-    
-    def contentType(self, file_name):
-        file_end = file_name.split(".")[-1]
-        if (file_end == "htm" or file_end == "html"):
-            return "text/html"
 
-        elif(file_end == "jpg"):            
-            return "image/jpeg"
-
-        elif(file_end == "png" or file_end == "gif"):
-            return "image/" + file_end
-
-
-        return "application/octet-stream"
-
+        print(f'-----------------------\n' \
+              f'Cliente: {self.cliente},\n' \
+              f'Servidor: {original_host},\n' \
+              f'Objeto requisitado: {objeto_requisitado},\n'
+              f'Código de resposta: {codigo_resposta}\n' 
+        )
 
 if __name__ == '__main__':
     server = WebServer()
